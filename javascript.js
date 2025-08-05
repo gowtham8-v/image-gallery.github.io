de57a59abe03dc170f7b21a3a1bee9c394348e6f
Gowtham <gowthamvenkat306@gmail.com>
2025-08-05 20:56:47 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,46 @@
+
+
+function showCat(){
+  if(document.querySelector('.cat-card').style.display === 'none'){
+    document.querySelector('.cat-card').style.display = 'block';
+  }
+  else{
+    document.querySelector('.cat-card').style.display = 'none';
+  }
+}
+
+function scrollAbt(){
+  const scroll = document.querySelector('.about');
+  scroll.scrollIntoView({behavior: "smooth"})
+  document.querySelector('.cat-card').style.display='none';
+}
+
+function scrollHome(){
+    const go = document.querySelector('.inside-content');
+  go.scrollIntoView({behavior: "smooth"})
+  document.querySelector('.cat-card').style.display='none';
+}
+
+function sub(event){
+  
+  alert('Submitted Your Message!');
+}
+
+function lightBox(img){
+  document.querySelector('.container').style.display = 'block';
+  const image = document.getElementById('show');
+  image.src = img.src;
+}
+
+function goway(){
+
+    document.querySelector('.container').style.display = 'none'
+
+  
+}
+
+function scrollLast(){
+  const last = document.querySelector('.last');
+  last.scrollIntoView({behavior: "smooth"});
+  document.querySelector('.cat-card').style.display='none';
+}
