6e5fd90bd421b530c42ece46930b04c73099d021
gowtham8-v <gowthamvenkat306@gmail.com>
2025-08-07 15:29:04 +0530

    full updated

--- javascript.js
+++ javascript.js
@@ -1,29 +1,5 @@
 
 
-function showCat(){
-  if(document.querySelector('.cat-card').style.display === 'none'){
-    document.querySelector('.cat-card').style.display = 'block';
-    document.querySelector('.menu-side').style.display = 'none';
-  }
-  else{
-    document.querySelector('.cat-card').style.display = 'none';
-    document.querySelector('.menu-side').style.display = 'none';
-  }
-}
-
-function scrollAbt(){
-  const scroll = document.querySelector('.about');
-  scroll.scrollIntoView({behavior: "smooth"})
-  document.querySelector('.cat-card').style.display='none';
-  document.querySelector('.menu-side').style.display = 'none';
-}
-
-function scrollHome(){
-    const go = document.querySelector('.inside-content');
-  go.scrollIntoView({behavior: "smooth"})
-  document.querySelector('.cat-card').style.display='none';
-  document.querySelector('.menu-side').style.display = 'none';
-}
 
 function sub(event){
   
@@ -41,13 +17,6 @@ function goway(){
     document.querySelector('.container').style.display = 'none'
 
   
-}
-
-function scrollLast(){
-  const last = document.querySelector('.last');
-  last.scrollIntoView({behavior: "smooth"});
-  document.querySelector('.cat-card').style.display='none';
-  document.querySelector('.menu-side').style.display = 'none';
 }
 
 function side(){
